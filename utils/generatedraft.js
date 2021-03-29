ec4aab8ef6f5057959457a20b30749f37ff2019f
Kaviilee <jiaweilee@126.com>
2021-03-30 01:52:41 +0800

    feat: new artical

--- utils/generatedraft.js
+++ utils/generatedraft.js
@@ -7,9 +7,9 @@ const cmdStr = `hexo new draft ${filename}`
 exec(cmdStr, (err, stdout) => {
   if (err) {
     console.error(err);
-    console.warn(new Date(), `创建 "${filename}" 失败`)
+    console.warn(new Date(), `创建文章 "${filename}" 失败`)
   } else {
     console.log(stdout);
-    console.info('\x1b[32m%s\x1b[0m', `创建 "${filename}" 成功`)
+    console.info('\x1b[32m%s\x1b[0m', `创建文章 "${filename}" 成功`)
   }
 })
